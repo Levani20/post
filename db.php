44fa1d0611572974eda2368f7917f824056cbf4e
levani <chanturialevani33@gmail.com>
2020-04-04 06:49:47 -0700

    css

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost", "chanturia", "nugzari123", "post");
+
+if(mysqli_connect_errno()) {
+	echo "Failed to connect to database";
+}
+?>
---
+++ home.php
@@ -0,0 +1,43 @@
+
+<html>
+  <head>
+    <title>PHP Test</title>
+    <link rel="stylesheet" href="home.css">
+  </head>
+  <body>
+      
+    <div id="navbar">
+			<div class="navbar-brand">7</div>
+			<input type="search" class="search-bar"/>
+			<div class="left-link-group">
+    		<li class="left-link">About</li>
+				<li class="left-link">vaja</li>
+				<li class="left-link">pshaveka</li>
+			</div>
+
+			<div class="right-link-group">
+				<li class="right-link">2</li>
+    		<li class="right-link">3</li>
+        <li class="right-link">4</li>
+      	<li class="right-link">5</li>
+      	<li class="right-link">6</li>
+      	<li class="right-link">7</li>
+			</div>
+        
+      
+		</div>
+
+
+
+		<div id="slider"></div>
+		<div id="gallery">
+    
+        
+
+		</div>		
+		<div id="footer"></div>
+
+
+		
+  </body>
+</html>
