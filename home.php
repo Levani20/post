ae843e895d5fc5b974264a543609aa14280e21c6
levani <chanturialevani33@gmail.com>
2020-04-09 06:24:24 -0700

    123

--- home.php
+++ home.php
@@ -1,43 +1,58 @@
-
 <html>
   <head>
     <title>PHP Test</title>
-    <link rel="stylesheet" href="home.css">
+	<link rel="stylesheet" href="home.css">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </head>
   <body>
       
     <div id="navbar">
-			<div class="navbar-brand">7</div>
-			<input type="search" class="search-bar"/>
+			<div class="navbar-brand">Blog <strong>Template</strong></div>
+			
 			<div class="left-link-group">
     		<li class="left-link">About</li>
-				<li class="left-link">vaja</li>
-				<li class="left-link">pshaveka</li>
+				<li class="left-link">cooperation</li>
+				<li class="left-link">contact</li>
 			</div>
 
 			<div class="right-link-group">
-				<li class="right-link">2</li>
-    		<li class="right-link">3</li>
-        <li class="right-link">4</li>
-      	<li class="right-link">5</li>
-      	<li class="right-link">6</li>
-      	<li class="right-link">7</li>
+				<li class="right-link">Nature</li>
+    		<li class="right-link">People</li>
+        <li class="right-link">Trips</li>
+      	<li class="right-link">Animals</li>
+      	<li class="right-link">Fashions</li>
+		  <li class="right-link">Tech</li>
+		  <li class="right-link">...</li>
 			</div>
-        
-      
 		</div>
-
-
-
-		<div id="slider"></div>
-		<div id="gallery">
     
-        
 
-		</div>		
-		<div id="footer"></div>
+		<header id="header" style="background-image: url('layer-12@2x.png');
+	background-size: cover;
+	background-repeat: no-repeat;">
+    
 
-
+				<img src="layer-4-copy-3@3x.png" alt="" class="qali">
+				<h3>Fill em with the venom and eliminate em other words, minute med em</h3>	
+		</header>
 		
+		<div id="gallery"></div>		
+		<div class="div13">
+            <div class="div16">
+                <h1 class="h15">Newsletter</h1>
+                <span class="span6">Donec elementum dui semper, pretium dui quis,pretium nisl.</span>
+            </div>
+        </div>
+		<div class="div14">
+            <div class="div15">
+                <span class="span7">Cum sociis natoque penabitus et magnis dis parturient montes, nascetur ridiculus mus.</span>
+                <span>Try awesome tool for dedsgners</span>
+                <span class="span8"> symu.co </span>
+            </div>
+        </div>
+		</div>		
   </body>
 </html>
