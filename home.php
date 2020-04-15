add1e88f40e551fb7376d82c88762a7bb1910fe4
levani <chanturialevani33@gmail.com>
2020-04-15 07:27:05 -0700

    davamtavre

--- home.php
+++ home.php
@@ -1,58 +1,154 @@
 <html>
-  <head>
-    <title>PHP Test</title>
+
+<head>
+	<title>PHP Test</title>
 	<link rel="stylesheet" href="home.css">
-	
+
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-      
-    <div id="navbar">
-			<div class="navbar-brand">Blog <strong>Template</strong></div>
-			
-			<div class="left-link-group">
-    		<li class="left-link">About</li>
-				<li class="left-link">cooperation</li>
-				<li class="left-link">contact</li>
-			</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 
-			<div class="right-link-group">
-				<li class="right-link">Nature</li>
-    		<li class="right-link">People</li>
-        <li class="right-link">Trips</li>
-      	<li class="right-link">Animals</li>
-      	<li class="right-link">Fashions</li>
-		  <li class="right-link">Tech</li>
-		  <li class="right-link">...</li>
-			</div>
+<body>
+
+	<div id="navbar">
+		<div class="navbar-brand">Blog <strong>Template</strong></div>
+
+		<div class="left-link-group">
+			<li class="left-link">About</li>
+			<li class="left-link">cooperation</li>
+			<li class="left-link">contact</li>
 		</div>
-    
 
-		<header id="header" style="background-image: url('layer-12@2x.png');
+		<div class="right-link-group">
+			<li class="right-link">Nature</li>
+			<li class="right-link">People</li>
+			<li class="right-link">Trips</li>
+			<li class="right-link">Animals</li>
+			<li class="right-link">Fashions</li>
+			<li class="right-link">Tech</li>
+			<li class="right-link">...</li>
+		</div>
+	</div>
+
+
+	<header id="header" style="background-image: url('layer-12@2x.png');
 	background-size: cover;
 	background-repeat: no-repeat;">
-    
 
-				<img src="layer-4-copy-3@3x.png" alt="" class="qali">
-				<h3>Fill em with the venom and eliminate em other words, minute med em</h3>	
-		</header>
-		
-		<div id="gallery"></div>		
-		<div class="div13">
-            <div class="div16">
-                <h1 class="h15">Newsletter</h1>
-                <span class="span6">Donec elementum dui semper, pretium dui quis,pretium nisl.</span>
-            </div>
-        </div>
-		<div class="div14">
-            <div class="div15">
-                <span class="span7">Cum sociis natoque penabitus et magnis dis parturient montes, nascetur ridiculus mus.</span>
-                <span>Try awesome tool for dedsgners</span>
-                <span class="span8"> symu.co </span>
-            </div>
-        </div>
-		</div>		
-  </body>
+
+		<img src="layer-4-copy-3@3x.png" alt="" class="qali">
+		<h3>Maecenas quis lobortis nunc. Nullam sit vel odio congue vulputate a ut nisi.</h3>
+	</header>
+
+
+
+	<div class="container">
+		<div class="row">
+			<!-- ori surati eqskalatori -->
+			<div class="post col-md-6 ">
+				<div class="post-image-block">
+					<img src="layer-3 (1).png" class="post-image">
+					<span class="post-image-lable9 post-image-lable-red9">Trips</span>
+				</div>
+				<h4 class="post-title">Donec elementum dui semper, pretium dui quis, pretium nisl. Nunc quis ornare odio.</h4>
+			</div>
+
+
+			<div class="post col-md-6 ">
+				<div class="post-image-block">
+					<img src="layer-3 (1).png" class="post-image">
+					<span class="post-image-lable">Tech</span>
+				</div>
+				<h4 class="post-title">Donec elementum dui semper, pretium dui quis, pretium nisl. Nunc quis ornare odio.</h4>
+			</div>
+		</div>
+		<!-- shua surati -->
+		<div class="row">
+			<div class="post col-md-12">
+				<div class="post-image-block">
+					<img src="layer-11 (1)." alt="mwvane" class="post-image">
+					<span class="post-image-lable1 post-image-lable-red1">Trips</span>
+					<h4 class="post-title">Proin congue elit fringilla
+						sodales tellus interdum fermentum nulla. Aliquam vitae arcu condimentum,
+						consectetur diam sed, ultrices urna. </h4>
+				</div>
+			</div>
+		</div>
+		<!-- 3 suratiani -->
+
+		<div class="row">
+			<div class="post col-md-4">
+				<div class="post-image-block">
+					<img src="layer-5 (1).png" alt="limonebi" class="post-image">
+					<span class="post-image-lable post-image-lable-green">Nature</span>
+					<h4>Donec elementum dui semper, pretium dui quis, pretium nisl.</h4>
+				</div>
+			</div>
+
+			<div class="post col-md-4">
+				<div class="post-img-block">
+					<img src="layer-7 (1).png" class="post-image" alt="wignebi">
+					<span class="post-image-lable post-image-lable-blue">People</span>
+					<h4>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</h4>
+				</div>
+			</div>
+
+			<div class="post col-md-4">
+				<div class="psot-img-block">
+					<img src="layer-6.png" class="post-image" alt="tower">
+					<span class="post-image-lable2">Tech</span>
+					<h4>Sed arcu ipsum, convallis quis porttitor bibendum, convallis non enim. </h4>
+				</div>
+			</div>
+		</div>
+
+		<!-- 3 suratiani2 -->
+
+		<div class="row">
+			<div class="post col-md-4">
+				<img src="layer-8.png" alt="3123" class="post-image">
+				<span class="post-image-lable post-image-lable-red">Trips</span>
+				<h4>Donec elementum dui semper, pretium dui quis, pretium nisl.</h4>
+			</div>
+
+
+			<div class="post col-md-4">
+				<img src="layer-9.png" alt="jancherito" class="post-image">
+				<span class="post-image-lable post-image-lable-red">Trips</span>
+				<h4>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</h4>
+			</div>
+
+			<div class="post col-md-4">
+				<img src="layer-10.png" alt="xiidi" class="post-image">
+				<span class="post-image-lable-purple">Fashion</span>
+				<h4>Sed arcu ipsum, convallis quis porttitor bibendum, convallis non enim.</h4>
+			</div>
+		</div>
+
+		<div class="row">
+			<!-- ori surati eqskalatori -->
+			<div class="post col-md-6 ">
+				<div class="post-image-block">
+					<img src="layer-3 (1).png" class="post-image">
+					<span class="post-image-lable9 post-image-lable-red9">Trips</span>
+				</div>
+				<h4 class="post-title">Donec elementum dui semper, pretium dui quis, pretium nisl. Nunc quis ornare odio.</h4>
+			</div>
+
+
+			<div class="post col-md-6 ">
+				<div class="post-image-block">
+					<img src="layer-3 (1).png" class="post-image">
+					<span class="post-image-lable">Tech</span>
+				</div>
+				<h4 class="post-title">Donec elementum dui semper, pretium dui quis, pretium nisl. Nunc quis ornare odio.</h4>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<h1 class="newsletter">Newsletter</h1>
+	</div>
+</body>
+
 </html>
